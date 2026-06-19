ddaf741c5ac32209d32f4c16032c0378f5f4cd7a
kukuhlisa <kuhlisyiharodiatun@gmail.com>
2026-06-19 16:37:27 +0700

    antrian update

--- app/Models/Antrian.php
+++ app/Models/Antrian.php
@@ -4,56 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Antrian extends Model
 {
     use HasFactory;
 
     protected $table = 'antrians';
+
     protected $fillable = [
-<<<<<<< HEAD
         'nama',
         'nim',
         'email',
         'whatsapp',
         'jenis_layanan',
         'kategori_layanan',
+        'metode_pembayaran',
         'waktu_layanan',
         'nomor_antrian',
         'tanggal_antrian',
         'status',
         'dokumen',
+        'bukti_transfer',
     ];
-=======
 
-'nama',
-
-'nim',
-
-'email',
-
-'whatsapp',
-
-'jenis_layanan',
-
-'kategori_layanan',
-
-'metode_pembayaran',
-
-'waktu_layanan',
-
-'nomor_antrian',
-
-'tanggal_antrian',
-
-'status',
-
-'bukti_transfer'
-
-];
->>>>>>> bfd8538b2193fc39ce995d9ff3121e16850c2355
-
-    // Relasi ke User (opsional, untuk dipanggil di blade jika dibutuhkan)
+    // Relasi ke User (berdasarkan email)
     public function user()
     {
         return $this->belongsTo(User::class, 'email', 'email');
