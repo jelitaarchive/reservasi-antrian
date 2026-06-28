166fb1ee7708ec14c039b526423b7a118cb7ae3e
kukuhlisa <kuhlisyiharodiatun@gmail.com>
2026-06-28 17:33:21 +0700

    up donlot riwayat pdf

--- app/Http/Controllers/RiwayatController.php
+++ app/Http/Controllers/RiwayatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Antrian;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class RiwayatController extends Controller
 {
@@ -33,5 +34,26 @@ class RiwayatController extends Controller
             'admin.riwayat-antrian',
             compact('riwayat')
         );
+    }
+    public function exportPdf(Request $request)
+    {
+        $query = Antrian::query();
+
+        if ($request->layanan) {
+            $query->where('jenis_layanan', $request->layanan);
+        }
+
+        $query->where('status', 'Selesai');
+
+        $riwayat = $query->latest()->get();
+
+        $pdf = Pdf::loadView(
+            'admin.riwayat-antrian-pdf',
+            compact('riwayat')
+        );
+
+        $pdf->setPaper('A4', 'landscape');
+
+        return $pdf->download('Riwayat_Antrian.pdf');
     }
 }
